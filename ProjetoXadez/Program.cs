79099cdec660d6ae300dfb94aa67454ade278150
Augusto Almeida <augusto.almeida@direction.com.br>
2021-11-29 14:28:32 -0300

    Refatorando a implementação do metodo imprimirTabuleiro

--- ProjetoXadez/Program.cs
+++ ProjetoXadez/Program.cs
@@ -8,22 +8,25 @@ namespace ProjetoXadez
     {
         static void Main(string[] args)
         {
-            //try
-            //{
-            //    Tabuleiro tab = new Tabuleiro(8, 8);
+            try
+            {
+                Tabuleiro tab = new Tabuleiro(8, 8);
 
-            //    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            //    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            //    tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(0, 2));
-            //    Tela.imprimirTabuleiro(tab);
-            //}catch(TabuleiroException e)
-            //{
-            //    Console.WriteLine(e.Message);
-            //}
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-            PosicaoXadrez pos = new PosicaoXadrez('c', );
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            //PosicaoXadrez pos = new PosicaoXadrez('c', 1);
+            //Console.WriteLine(pos);
+            //Console.WriteLine(pos.toPosicao());
 
             Console.ReadLine();
         }
