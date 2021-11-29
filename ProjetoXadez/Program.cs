cbe835e7c69ec77b493d245dd869dd8872f61d75
Augusto Almeida <augusto.almeida@direction.com.br>
2021-11-29 10:33:28 -0300

    Criado base da classe Posição.

---
+++ ProjetoXadez/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Tabuleiro;
+
+namespace ProjetoXadez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao P;
+            P = new Posicao(3, 4);
+            Console.WriteLine(P);
+            Console.ReadLine();
+        }
+    }
+}
